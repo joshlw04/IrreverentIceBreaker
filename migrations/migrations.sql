31d36203e4dc32c0d4a7e6aa92dea44960f8c0ec
Elizabeth Cheng <echeng924@gmail.com>
2016-10-09 14:49:11 -0400

    add initial migrations

---
+++ migrations/migrations.sql
@@ -0,0 +1,18 @@
+--update your .env file for database URL name
+
+DROP DATABASE IF EXISTS irreverent_ice_breaker;
+CREATE DATABASE irreverent_ice_breaker;
+
+/c irreverent_ice_breaker
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  email VARCHAR NOT NULL,
+  password VARCHAR NOT NULL
+);
+
+CREATE TABLE questions (
+  id SERIAL PRIMARY KEY,
+  question VARCHAR(60) NOT NULL,
+  type VARHAR(60) NOT NULL
+)
