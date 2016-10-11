225bac252789af89a417f83647777123def09831
Elizabeth Cheng <echeng924@gmail.com>
2016-10-11 11:17:08 -0400

    updated seed file for light dark naming

--- migrations/seed.sql
+++ migrations/seed.sql
@@ -3,10 +3,10 @@ TRUNCATE TABLE QUESTIONS;
 
 INSERT INTO admins (email, password) VALUES ('liz@test.com', 'password');
 
-INSERT INTO questions (question, type) VALUES ('What did you have for dinner?', 'hard');
-INSERT INTO questions (question, type) VALUES ('Who are you voting for?', 'hard');
-INSERT INTO questions (question, type) VALUES ('Where did you grow up?', 'hard');
+INSERT INTO questions (question, type) VALUES ('What did you have for dinner?', 'dark');
+INSERT INTO questions (question, type) VALUES ('Who are you voting for?', 'dark');
+INSERT INTO questions (question, type) VALUES ('Where did you grow up?', 'dark');
 
-INSERT INTO questions (question, type) VALUES ('Where did you last travel?', 'easy');
-INSERT INTO questions (question, type) VALUES ('What is your favorite color?', 'easy');
-INSERT INTO questions (question, type) VALUES ('How many siblings do you have?', 'easy');
+INSERT INTO questions (question, type) VALUES ('Where did you last travel?', 'light');
+INSERT INTO questions (question, type) VALUES ('What is your favorite color?', 'light');
+INSERT INTO questions (question, type) VALUES ('How many siblings do you have?', 'light');
