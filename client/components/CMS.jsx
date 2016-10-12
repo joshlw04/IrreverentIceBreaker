9e13860a963883a3c4433d5eae27fe90baf6b46a
Elizabeth Cheng <echeng924@gmail.com>
2016-10-12 15:38:56 -0400

    completed cms

--- client/components/CMS.jsx
+++ client/components/CMS.jsx
@@ -10,6 +10,10 @@ class CMS extends Component {
     this.state = {
       questions:[],
     };
+    this.handlePublish = this.handlePublish.bind(this);
+    this.httpUpdateQuestion = this.httpUpdateQuestion.bind(this);
+    this.httpPublishQuestion = this.httpPublishQuestion.bind(this);
+    this.httpDeleteQuestion = this.httpDeleteQuestion.bind(this);
   }
   componentDidMount() {
     this.getQuestions();
@@ -25,7 +29,8 @@ class CMS extends Component {
                 questions = Object.keys(questionData).map((id) => {
                   const indvQuestionData = questionData[id];
                   return {
-                    id,
+                    // id,
+                    id: indvQuestionData.id,
                     question: indvQuestionData.question,
                     type: indvQuestionData.type,
                   };
@@ -35,24 +40,47 @@ class CMS extends Component {
            });
   }
 
-  check() {
-    // setTimeout(function() {
-    const check = this.state.questions.map((individualQuestion) => {
-      return (
-        individualQuestion.question
-      );
-    });
-    console.log(check);
-  //   }, 1000);
+  handlePublish({ id, type, question }) {
+    if (id) {
+      this.httpUpdateQuestion({ id, type, question });
+    } else {
+      this.httpPublishQuestion({ type, question });
+    }
+  }
+  httpDeleteQuestion(id) {
+    // const url = `https://meerkats-e16d1.firebaseio.com/posts/${id}.json`;
+    // request.del(url)
+    // request.del(url)
+    request.del(`/api/questions/${id}`)
+           .then(() => {
+             this.getQuestions();
+           });
+  }
+  httpUpdateQuestion({ id, type, question }) {
+    // const url = `https://meerkats-e16d1.firebaseio.com/posts/${id}.json`;
+    // request.patch(url)
+    request.put(`/api/questions/${id}`)
+           .send({ type, question })
+           .then(() => {
+             this.getQuestions();
+           });
+  }
+  httpPublishQuestion({ type, question }) {
+    // const url = 'https://meerkats-e16d1.firebaseio.com/posts.json';
+    // request.post(url)
+    request.post('/api/questions')
+           .send({ type, question })
+           .then(() => {
+             this.getQuestions();
+           });
   }
 
   render() {
-    this.check();
     return (
       <div>
         test
-        <QuestionList questions={this.state.questions} />
-        <CMSQuestion />
+        <QuestionList handleDelete={this.httpDeleteQuestion} handlePublish={this.handlePublish} questions={this.state.questions} />
+        <CMSQuestion handleDelete={this.httpDeleteQuestion} handlePublish={this.handlePublish} />
       </div>
     );
   }
--- client/components/CMS/questionList.jsx
+++ client/components/CMS/questionList.jsx
@@ -2,34 +2,33 @@ import React, { Component } from 'react';
 import CMSQuestion from './cmsQuestion.jsx';
 
 const propTypes = {
+  handleDelete: React.PropTypes.func,
+  handlePublish: React.PropTypes.func,
   questions: React.PropTypes.array,
 };
 
+// handleDelete={this.httpDeleteQuestion} handlePublish={this.handlePublish} questions={this.state.questions}
 
-// console.log(this.props);
 class QuestionList extends Component {
-
-    // componentDidMount() {
-    //   setTimeout((function) => {
-    //     console.log(this.props.questions);
-    //   }, 1000)
-    // }
-
   render() {
-    // const questionElements = this.props.questions.map((individualQuestion) => {
-    //   return (
-    //     <li>
-    //       <CMSQuestion
-    //         id: {individualQuestion.id}
-    //         question: {individualQuestion.question}
-    //         type: {individualQuestion.type}
-    //       />
-    //     </li>
-    //   )
-    // })
+    const questionElements = this.props.questions.map((individualQuestion, idx) => {
+      return (
+        <li key={idx}>
+        {/* {individualQuestion.question} */}
+        {/* {individualQuestion.type} */}
+          <CMSQuestion
+            handleDelete={this.props.handleDelete}
+            handlePublish={this.props.handlePublish}
+            type={individualQuestion.type}
+            question={individualQuestion.question}
+            id={individualQuestion.id}
+           />
+        </li>
+      );
+    });
     return (
       <ul>
-
+        {questionElements}
       </ul>
     );
   }
