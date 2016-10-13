73aa11cf41d9fda8ebdb257f7acda5dc6f201115
Josh Whitney <josh.whitney18@gmail.com>
2016-10-13 11:19:44 -0400

    add background div for each question p tag

--- client/components/Question.jsx
+++ client/components/Question.jsx
@@ -10,32 +10,35 @@ class Question extends Component {
     super();
   }
   render() {
-    const lightStyle = {
-      backgroundColor: 'white',
-      color: 'blue',
+    // const lightStyle = {
+      // backgroundColor: 'white',
+      // color: 'blue',
       // width: '80%',
       // margin: '2em auto',
       // padding: '1em',
       // fontSize: '3em'
-    };
-    const darkStyle = {
-      backgroundColor: 'black',
-      color: 'red',
+    // };
+    // const darkStyle = {
+      // backgroundColor: 'black',
+      // color: 'red',
       // width: '80%',
       // margin: '2em auto',
       // padding: '1em',
       // fontSize: '3em'
-    };
+    // };
     return (
       <div>
       {this.props.questionType === 'light' ?
           <div className='lightQuestionDiv'>
             {this.props.currentQuestion}
+          <div className='lightQuestionDivBackground'></div>
           </div>
         :
         <div className='darkQuestionDiv'>
             {this.props.currentQuestion}
-        </div>}
+        <div className='darkQuestionDivBackground'></div>
+        </div>
+        }
     </div>
     );
   }
