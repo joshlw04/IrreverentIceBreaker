0ee542e7b91fdfda2f3a2b87377676a75a6c69e2
Nasser Sanou <wardjamnaty@yahoo.fr>
2016-10-13 08:06:10 -0400

    fixed Authentication

--- app/config/sqlProvider.js
+++ app/config/sqlProvider.js
@@ -8,17 +8,17 @@ function sql(file) {
 
 const sqlProvider = {
   admins: {
-    all: sql('./sql/user/all.sql'),
-    find: sql('./sql/user/find.sql'),
-    create: sql('.sql/user/create.sql'),
-    delete: sql('./sql/user/delete.sql'),
+    all: sql('/sql/admin/all.sql'),
+    find: sql('/sql/admin/find.sql'),
+    create: sql('sql/admin/create.sql'),
+    delete: sql('/sql/admin/delete.sql'),
   },
   questions: {
-    all: sql('./sql/question/all.sql'),
-    find: sql('./sql/question/find.sql'),
-    create: sql('./sql/question/create.sql'),
-    update: sql('./sql/question/update.sql'),
-    delete: sql('./sql/question/delete.sql'),
+    all: sql('/sql/question/all.sql'),
+    find: sql('/sql/question/find.sql'),
+    create: sql('/sql/question/create.sql'),
+    update: sql('/sql/question/update.sql'),
+    delete: sql('/sql/question/delete.sql'),
   },
 };
 
